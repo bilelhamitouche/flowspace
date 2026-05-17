514e3bd67b0fe4a40f7853679a4269c76c278141
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2026-05-17 02:10:30 +0100

    feat: add custom fetch function

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -4,3 +4,32 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const apiFetch = async (path: string, options?: RequestInit) => {
+  let res = await fetch(`${import.meta.env.VITE_API_URL}${path}`, {
+    credentials: "include",
+    ...options,
+  });
+
+  if (res.status === 401) {
+    const refresh = await fetch(
+      `${import.meta.env.VITE_API_URL}/api/auth/refresh`,
+      {
+        method: "POST",
+        credentials: "include",
+      },
+    );
+    if (refresh.ok) {
+      res = await fetch(`${import.meta.env.VITE_API_URL}${path}`, {
+        credentials: "include",
+        ...options,
+      });
+    } else {
+      throw new Error("Unauthenticated");
+    }
+  }
+
+  if (!res.ok) throw new Error(await res.text());
+  const text = await res.text();
+  return text ? JSON.parse(text) : null;
+};
