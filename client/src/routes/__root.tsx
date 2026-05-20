320da68d275e179c84cdf3460a530f53ae948e24
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2026-05-20 16:21:52 +0100

    feat: add global error and not found pages

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,9 +1,17 @@
+import ErrorComponent from "@/components/error";
+import NotFoundComponent from "@/components/not-found";
 import type { RouterContext } from "@/types/router-context";
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRouteWithContext<RouterContext>()({
   component: RootComponent,
+  notFoundComponent: () => {
+    return NotFoundComponent();
+  },
+  errorComponent: ({ error }) => {
+    return ErrorComponent(error);
+  },
 });
 
 export function RootComponent() {
