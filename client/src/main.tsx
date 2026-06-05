18aa2063e817aecdfb2d64db1c5dbf87aa5ff500
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2026-06-05 05:18:56 +0100

    fix: strict mode import from react

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { queryClient, router } from "./lib/router";
@@ -6,6 +5,7 @@ import { TooltipProvider } from "./components/ui/tooltip";
 import "./index.css";
 import { Toaster } from "./components/ui/sonner";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { StrictMode } from "react";
 
 declare module "@tanstack/react-router" {
   interface Register {
