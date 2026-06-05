a0689128e8833caf8212183246b4e05942c733a0
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2026-06-05 05:19:45 +0100

    feat: add settings pages

---
+++ client/src/routes/settings/appearance/index.tsx
@@ -0,0 +1,9 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/settings/appearance/')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return <div>Hello "/settings/appearance/"!</div>
+}
