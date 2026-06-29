a9b939b62b1e322cdc93831f4c47b62885c0aba9
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2026-06-29 04:04:42 +0100

    chore: clean up unneeded resources

--- server/src/tasks/dto/update-task.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTaskDto } from './create-task.dto';
-import { IsOptional, IsString } from 'class-validator';
-
-export class UpdateTaskDto extends PartialType(CreateTaskDto) {
-  @IsOptional()
-  @IsString()
-  listId?: string;
-}
